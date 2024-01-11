6e00b6b231fba4a63ed84ad5164012bdc7ac3356
Parisa-Norouzi <parisanor6@gmail.com>
2024-01-11 23:28:26 +0330

    add creat table for authours

---
+++ src/main/java/table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE authours (
+                          Authour_id serial primary key ,
+                          First_Name varchar(255),
+                          Last_Name varchar(255),
+                          age int
+);
