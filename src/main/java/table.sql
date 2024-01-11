d345a2808ce79ac58ec4dd7a32633489cc3638b9
Parisa-Norouzi <parisanor6@gmail.com>
2024-01-12 00:55:29 +0330

    creat class authour

--- src/main/java/table.sql
+++ src/main/java/table.sql
@@ -4,3 +4,9 @@ CREATE TABLE authours (
                           Last_Name varchar(255),
                           age int
 );
+CREATE TABLE book (
+                          Book_id serial primary key ,
+                          Book_Name varchar(255),
+                          AuthourRef varchar(255),
+                          year_publish int
+);
