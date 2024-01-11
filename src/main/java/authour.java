dcf8573c928152ace6ddcd09f20887d2da59e81e
Parisa-Norouzi <parisanor6@gmail.com>
2024-01-12 01:00:17 +0330

    add set, get to authour

--- src/main/java/authour.java
+++ src/main/java/authour.java
@@ -1,2 +1,59 @@
 public class authour {
+    private int Authourid ;
+   private String FirstName ;
+    private String  LastName;
+   private int age ;
+
+    public authour() {
+
+    }
+
+    public authour(int authourid, String firstName, String lastName, int age) {
+        Authourid = authourid;
+        FirstName = firstName;
+        LastName = lastName;
+        this.age = age;
+    }
+
+    public int getAuthourid() {
+        return Authourid;
+    }
+
+    public void setAuthourid(int authourid) {
+        Authourid = authourid;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "authour{" +
+                "Authourid=" + Authourid +
+                ", FirstName='" + FirstName + '\'' +
+                ", LastName='" + LastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
