92bbe387b01c8d12bf9a69982a391a754212015a
Parisa-Norouzi <parisanor6@gmail.com>
2024-01-12 12:48:32 +0330

    add JdbcConnection  CLASS

---
+++ src/main/java/JdbcConnection.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcConnection {
+
+    private final Connection connection =
+            DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "1234");
+
+    public JdbcConnection() throws SQLException {
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
